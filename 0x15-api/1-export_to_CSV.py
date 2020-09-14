1fb78882a213254027cce9aa99fb2c5e48f547d5
zacwoll <zacwoll@gmail.com>
2020-09-14 23:14:24 +0000

    switched to csv export

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,6 +3,7 @@
 For a given employee ID, return information about his/her TODO progress
 """
 if __name__ == '__main__':
+    import csv
     import requests
     from sys import argv
 
@@ -11,6 +12,9 @@ if __name__ == '__main__':
     uname = r.json().get('username')
     r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
                      .format(argv[1]))
-    for _ in r.json():
-        print(",".join([argv[1], uname, str(_.get('completed')),
-                        _.get('title')]))
+    with open('{}.csv'.format(argv[1]), mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for _ in r.json():
+            csv_writer.writerow([argv[1], uname, _.get('completed'),
+                           _.get('title')])
