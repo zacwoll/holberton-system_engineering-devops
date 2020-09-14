cdbd8bab0a768a4fed0b001466e2814091344123
zacwoll <zacwoll@gmail.com>
2020-09-14 23:10:07 +0000

    put them in if name is mains

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""For a given employee ID, return information about his/her TODO progress"""
-import requests
-from sys import argv
+"""
+For a given employee ID, return information about his/her TODO progress
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
 
-
-r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                 .format(argv[1]))
-uname = r.json().get('username')
-r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                 .format(argv[1]))
-for _ in r.json():
-    print(",".join([argv[1], uname, str(_.get('completed')), _.get('title')]))
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(argv[1]))
+    uname = r.json().get('username')
+    r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                     .format(argv[1]))
+    for _ in r.json():
+        print(",".join([argv[1], uname, str(_.get('completed')),
+                        _.get('title')]))
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""For a given employee ID, export information about his/her TODO progress"""
-import json
-import requests
-from sys import argv
+"""
+For a given employee ID, export information about his/her TODO progress
+"""
+if __name__ == '__main__':
+    import json
+    import requests
+    from sys import argv
 
 
-r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                 .format(argv[1]))
-uname = r.json().get('username')
-r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                 .format(argv[1]))
-output = {}
-tasks = [{"task": _.get('title'), "completed": _.get('completed'), "username":
-         uname} for _ in r.json()]
-output = {argv[1]: tasks}
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(argv[1]))
+    uname = r.json().get('username')
+    r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                     .format(argv[1]))
+    output = {}
+    tasks = [{"task": _.get('title'), "completed": _.get('completed'), "username":
+             uname} for _ in r.json()]
+    output = {argv[1]: tasks}
 
-with open('{}.json'.format(argv[1]), 'w') as outfile:
-    json.dump(output, outfile)
+    with open('{}.json'.format(argv[1]), 'w') as outfile:
+        json.dump(output, outfile)
