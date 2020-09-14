f2bef7c8383e768b5cd78bbbe8b12c6e5fb6633a
zacwoll <zacwoll@gmail.com>
2020-09-14 22:55:06 +0000

    put up if name is main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,12 +4,13 @@ import requests
 from sys import argv
 
 
-r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+if __name__ == '__main__':
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                  .format(argv[1]))
-name = r.json().get('name')
-r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                 .format(argv[1]))
-print("Employee {} is done with tasks({}/{})".format(
-    name, [.get('completed') for _ in r.json()].count(True), len(r.json())))
-for _ in r.json():
-    print("".join(["\t ", _.get('title')]))
+    name = r.json().get('name')
+    r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                     .format(argv[1]))
+    print("Employee {} is done with tasks({}/{})".format(
+        name, [.get('completed') for _ in r.json()].count(True), len(r.json())))
+    for _ in r.json():
+        print("".join(["\t ", _.get('title')]))
