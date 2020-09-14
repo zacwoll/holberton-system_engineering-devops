07774b3afcc6aa626f08668da80701eeedc91bae
zacwoll <zacwoll@gmail.com>
2020-09-14 23:02:00 +0000

    at this point i'll slowly make my code with more and more lines until it passes checker

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,9 +12,14 @@ if __name__ == '__main__':
     name = r.json().get('name')
     r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
                      .format(argv[1]))
-    print("Employee {} is done with tasks({}/{})".format(
-        name, [_.get('completed') for _ in r.json()].count(True),
-        len(r.json())))
+    completed = total = 0
+    for task in r.json():
+        total += 1
+        if task.get('completed'):
+            done += 1
+
+    print("Employee {} is done with tasks({}/{})".format(name, completed,
+                                                         total))
     for _ in r.json():
         if _.get('completed'):
             print("".join(["\t ", _.get('title')]))
