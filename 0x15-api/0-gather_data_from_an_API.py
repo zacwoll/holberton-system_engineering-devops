ceae7ec5efb3d0df242e7b22de4263d7752cee39
zacwoll <zacwoll@gmail.com>
2020-09-14 22:57:26 +0000

    fixed a typo, pycodestyled again

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""For a given employee ID, return information about his/her TODO progress"""
-import requests
-from sys import argv
-
+"""
+For a given employee ID, return information about his/her TODO progress
+"""
 
 if __name__ == '__main__':
+    import requests
+    from sys import argv
+
     r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                 .format(argv[1]))
+                     .format(argv[1]))
     name = r.json().get('name')
     r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
                      .format(argv[1]))
     print("Employee {} is done with tasks({}/{})".format(
-        name, [.get('completed') for _ in r.json()].count(True), len(r.json())))
+        name, [_.get('completed') for _ in r.json()].count(True),
+        len(r.json())))
     for _ in r.json():
         print("".join(["\t ", _.get('title')]))
